64773cce2d610f7c5d3ecb83e58f113104158405
MrDinosaur666 <78523157+MrDinosaur666@users.noreply.github.com>
2021-02-04 11:04:40 +0530

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -9,7 +9,7 @@ weather.temperature = {
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 12.880621;
+  let longitude = 80.202301;
 
   getWeather(latitude, longitude);
 }
