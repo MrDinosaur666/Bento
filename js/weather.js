2a80bed2be62b9bc574fac489d6292167992ad1f
MrDinosaur666 <78523157+MrDinosaur666@users.noreply.github.com>
2021-02-04 11:57:35 +0530

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87441928fb3cc32d3d72';
+const key = '62730a8ccabb98b59705816f79b0a1bd';
 
 // Set Position function
 setPosition();
